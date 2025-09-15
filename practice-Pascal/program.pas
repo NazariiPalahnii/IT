88746897ce546c4084065ae3d73241345403b047
Nazarii Palahhnii <nazariipalahnii@gmail.com>
2025-09-15 06:50:04 +0200

    pascal

---
+++ practice-Pascal/program.pas
@@ -0,0 +1,19 @@
+// program ArithmeticDemo;
+//
+// var 
+//   num1,num2: integer;
+//   result: real;
+//
+// begin 
+//   num1 := 10;
+//   num2 := 3;
+//
+//   result := num1 / num2; 
+//   writeln('10 / 3 = ', result:0:2);
+//
+//   writeln('10 div 3 = ', num1 div num2);
+//   writeln('10 div 3 = ', num1 mod num2);
+//   readln;
+// end.
+
+
