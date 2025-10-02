5e4c9fba0ed71c8d696f7fcd6ac8d972d2cc475c
Nazarii Palahhnii <nazariipalahnii@gmail.com>
2025-10-02 06:49:05 +0200

    website

---
+++ website_python/run.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    # print(url_for('index'))
+    return render_template(
+        'index.html', 
+        titleOfWebsite='My Portfolio',
+        headerTitle='Prizrak_幽霊'
+    )
+
+if __name__ == '__main__':
+    app.run(debug=True)
