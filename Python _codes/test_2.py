b4f9f274f97c9b37bef8c9614333bec42d4da4b8
Nazarii Palahhnii <nazariipalahnii@gmail.com>
2025-10-04 22:09:02 +0200

    update

--- Python _codes/test_2.py
+++ Python _codes/test_2.py
@@ -1,15 +1,30 @@
-nums = [2,3,1,1]
+import glob
+listFile = []
+def find_files(pattern):
 
+     # finding all files matching the pattern
+    return glob.glob(pattern, recursive=True)
 
-def Number(nums):
-    for i in range(len(nums)):
-        for j in range(i + 1, len(nums)):
-            if nums[j] == nums[i] and j != i:
-                return True
-        
-        return False
+files = find_files('/home/laptop_Prizrak/MyHobby/Python _codes/test.py')
+files1 = find_files('/home/laptop_Prizrak/MyHobby/Python _codes/test_1.py')
 
+for file in files:
+    print(file)
+    with open(file, 'r') as file:
+        content = file.read()
+        listFile.append(content)
 
-print(Number(nums))
-
-
+for file1 in files1:
+    with open(file1, 'r') as file1:
+        content = file1.read()
+        listFile.append(content)
+         
+j = 0
+for i in range(1, len(listFile)):
+    if listFile[i] == listFile[j]:
+        print('it is the same content')
+        break
+    else:
+        print('it is not the same content')
+        break
+    j = i
