bc95aa78bef9c4193b1433863b60ceac35769f1f
Nazarii Palahhnii <nazariipalahnii@gmail.com>
2025-10-09 22:24:21 +0200

    update

---
+++ Python _codes/test_3.py
@@ -0,0 +1,53 @@
+import os
+import glob
+
+user = "Prizrak"
+userLaptop = "laptop_Prizrak"
+
+## FUNCTION FOR FIND MY FILES
+def find_files(pattern):
+    return glob.glob(pattern, recursive=True)
+
+## MY PATHS TO FILRS
+files = find_files(f'/home/{user}/IT/Python _codes/*.py')
+files1 = find_files(f'/home/{user}/IT/Python _codes/*.py')
+
+## FUNCTION TO MAKE A LIST NAMES OF MY FILES
+def nameFile(file_paths):
+    nameList = []
+    for name in file_paths:
+        namePath = os.path.basename(name)
+        nameList.append(namePath)
+
+    return nameList
+
+## FUNCTION TO OPEN MY FILES BY THE PATH 
+def openFile(file_paths):
+    contentList = []
+    for path in file_paths:
+        try:
+            with open(path, 'r') as f:
+                content = f.read()
+                contentList.append(content)
+        except Exception as e:
+            print(f'I dont find the file: {path}: {e}')
+
+    return contentList
+    
+
+old = openFile(files)
+
+new = openFile(files1)
+
+nameOld = nameFile(files)
+nameNew = nameFile(files1)
+
+
+for j in range(len(nameNew)):
+    for i in range(2, len(nameOld)):
+        if nameNew[j] == nameOld[i]:
+           os.system('') 
+        else:
+            print(nameNew[j], nameOld[i], False)
+            break
+
